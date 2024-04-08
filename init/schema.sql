eb35c9c9181d994dc8a26361b9def0a3cc60f045
romeucajamba <romeucajamba@gmail.com>
2024-04-08 01:08:30 +0100

    config: resolve the error in db sql

--- init/schema.sql
+++ init/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE "attendees"(
 CREATE TABLE "check_ins"(
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "atendeeID" TEXT NOT NULL,
+    "attendeeId" TEXT NOT NULL,
     CONSTRAINT "check_ins_attendeeId_fkey" FOREIGN KEY ("attendeeId") REFERENCES "attendeeId" ('id') ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
