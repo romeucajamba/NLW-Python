d73e3e220d9a70025f5ca010963fa2f5bebe1a7f
romeucajamba <romeucajamba@gmail.com>
2024-04-08 01:36:35 +0100

    feat: implement connection with db

--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -1,6 +1,7 @@
 ###### Conexão com Banco de Dados ###
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
     def __init__(self) -> None:
@@ -9,8 +10,18 @@ class DBConnectionHandler:
             "storage.db"
         ) 
         self.__engine = None
+        self.__session = None
     def connector_db(self) -> None:
         self.__engine = create_engine(self.connection_string)
 
     def get_egine(self):
         return self.__engine
+    
+    def __enter__(self):
+        session_maker = sessionmaker()
+        self.__session = session_maker(bind=self.__engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__session.close()
+        self.__engine.dispose()
